c1837037ef0bce13688ad78c638bb3f3c219005a
wsion <waychanmail@gmail.com>
2016-11-04 20:54:03 +0800

    Pending pagination.

--- Web/app/controller/empController.js
+++ Web/app/controller/empController.js
@@ -4,25 +4,38 @@ app.controller("empCtrl", function ($scope, $location, $http, eHRSettings) {
     var vm = this;
 
     vm.DataSet = [];
-
-    vm.viewMode = 'no-data';
+    vm.PageSizes = [2, 5, 10, 20];
+    vm.PageSize = vm.PageSizes[0];
+    vm.PageCount = 0;
+    vm.CurrentPage = 1;
+    vm.PageRange = [];
+    vm.ViewMode = 'no-data';
 
     vm.loadData = function () {
         vm.viewMode = 'data-loading';
-        var url = eHRSettings.baseUri + "Employee?$expand=Department";
+        var url = eHRSettings.baseUri + "Employee?$expand=Department&$inlinecount=allpages"; //todo:pagination, $top=1&$skip=1
         $http.get(url)
         .success(function (response) {
             if (response.value != undefined && response.value.length > 0) {
                 vm.DataSet = response.value;
-                vm.viewMode = 'data-loaded';
+                vm.ViewMode = 'data-loaded';
+                vm.PageCount = Math.round(response["odata.count"] / vm.PageSize);
+                vm.CurrentPage = 1;
+                vm.PageRange = new Array(vm.PageCount);
             }
         })
         .error(function () {
-            vm.viewMode = 'no-data';
+            vm.ViewMode = 'no-data';
         });
     };
 
     vm.loadData();
+
+    vm.OnPageSizeChange = function (size) {
+        if (vm.PageSizes.indexOf(size) > -1) {
+            vm.PageSize = size;
+        }
+    };
 
     console.log($scope);
 });
